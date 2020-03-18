e4ab2b5ec5c28951e205801386a2a08e3b340d30
Abhaya Nigam <abhaya.nigam13@gmail.com>
2020-03-18 17:31:11 +0530

    understand

--- output.c
+++ output.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 //program of ps
-void printer(int lines);
-void printerline(int i);
+void printer(int lines);//declaration
+void printerline(int i);//declaration
 
 int main()
 {
 	
-	printer(10);
+	printer(10);//call form the function
 	return 0;	
 }
-
+//implimentation the function
 void printer(int lines){
  	
 	for (int i = 0; i < lines; ++i)
 	{
 
-   	 printerline(i);
-
+   	 printerline(i);//call from the function
 	}	
 
 }
+//implimentation 
 void printerline(int i)
 {
 	char a='*';
